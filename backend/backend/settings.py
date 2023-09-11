411ad164c18f65355b58278abdaab7a16f466b99
Илья Пушкарский <i.pushkars@yandex.ru>
2023-09-11 18:54:45 +0300

    123

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -101,7 +101,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 STATIC_ROOT = BASE_DIR / 'collected_static'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
